7c68446f8cacf41f618bc51467b6a591d968e899
rriva <rriva@fbk.eu>
2026-07-01 14:54:41 +0200

    logger and callbacks instatiators like Hydra-Lightning-Template, notebook to start testing and developing Pretrainig LModule

--- src/fm4tag/utils/__init__.py
+++ src/fm4tag/utils/__init__.py
@@ -1,3 +1,8 @@
-from hydra.utils import instantiate
+from .instantiators import instantiate_callbacks, instantiate_loggers
+from .pylogger import RankedLogger
 
-__all__ = ['instantiate']
+__all__ = [
+    "instantiate_callbacks",
+    "instantiate_loggers",
+    "RankedLogger"
+]
